9cf870603c42209195fd49cb7d94ff01e9c42af9
Brandon Myers <brandonstrash91@yahoo.com>
2022-02-06 16:48:06 -0500

    add coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import Leaderboard from './Leaderboard';
 import Questionaire from './Questionaire';
+import { comingSoon } from './data/config';
+import ComingSoon from './ComingSoon';
 
 function App() {
-  let name = localStorage.getItem('name');
+  let name = localStorage.getItem('name56');
   useEffect(() => {
     if (window.location.search.indexOf('clear') >= 0) {
-      localStorage.removeItem('name');
+      localStorage.removeItem('name56');
       name = null;
       window.location.href = window.location.origin;
     }
   }, [name]);
 
-  return (
+  return comingSoon ? (
+    <ComingSoon />
+  ) : (
     <div className="App">
       <header className="App-header">
-        <h1>Superbowl LV Prop Bets</h1>
+        <h1>Superbowl LVI Prop Bets</h1>
       </header>
       {name ? <Leaderboard name={name} /> : <Questionaire />}
     </div>
---
+++ src/ComingSoon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ComingSoon = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100%',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Superbowl LVI Prop Bets</h1>
+      <h2>🎉Coming Soon!🎉</h2>
+      <p style={{ marginTop: 24 }}>
+        Check back on Super Bowl Sunday to make your choices
+      </p>
+    </div>
+  );
+};
+
+export default ComingSoon;
